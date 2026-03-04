3f0a6c18d8e9f6b62fdd4275fa958931aed2c456
Syed Sadiq Ahamed <syedsadiqahamed198@gmail.com>
2026-03-04 17:17:44 +0530

    Basics

---
+++ Basics/areaofcircle.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+public class areaofcircle {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        float pi=sc.nextFloat();
+        int r=sc.nextInt();
+        float area=pi*r*r;
+        System.out.println(area);
+
+
+    }
+}
