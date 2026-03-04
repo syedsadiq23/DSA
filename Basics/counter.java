3db116cbb48dfafaf59675b810992b58fff9f68a
Syed Sadiq Ahamed <syedsadiqahamed198@gmail.com>
2026-03-04 17:22:23 +0530

    Basics

---
+++ Basics/counter.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class counter {
+    public static void main(String args[]){
+        int counter=0;
+        while(counter<100){
+            System.out.println("Hello world");
+            counter++;
+        }
+    }
+
+}
