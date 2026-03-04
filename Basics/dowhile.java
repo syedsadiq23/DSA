b3ff52801c3e1cfc64cb3c9b5e2de9bb41d56fa3
Syed Sadiq Ahamed <syedsadiqahamed198@gmail.com>
2026-03-04 17:22:56 +0530

    Basics

---
+++ Basics/dowhile.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class dowhile {
+    public static void main(String args[]){
+        int counter=1;
+        do{
+            System.out.print("Hello World");
+            counter++;
+        }while(counter<=10);
+    }
+    
+}
