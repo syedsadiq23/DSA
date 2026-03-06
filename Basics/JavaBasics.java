024ff8d1dcbdd24939ac48411b753bb9e8b098ad
Syed Sadiq Ahamed <syedsadiqahamed198@gmail.com>
2026-03-06 14:17:23 +0530

    Basics

---
+++ Basics/JavaBasics.java
@@ -0,0 +1,11 @@
+public class JavaBasics {
+    public static void main(String args[]){
+       System.out.print("My name is Syed Sadiq Ahamed ");
+       System.out.println("****");
+       System.out.println("***");
+       System.out.println ("**");
+       System.out.println("*");
+        
+       
+    }
+}
