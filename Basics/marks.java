5bafd2c137e16ff22f53c09fd0c61496787731f3
Syed Sadiq Ahamed <syedsadiqahamed198@gmail.com>
2026-03-07 23:23:12 +0530

    Basics

---
+++ Basics/marks.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class marks {
+    public static void main(String args[]){
+        int marks=45;
+        String result=(marks>=33)?"Pass":"Fail";
+        System.out.println(result);
+    }
+    
+}
