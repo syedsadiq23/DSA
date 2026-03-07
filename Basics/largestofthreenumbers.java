e4a77b13dde892e5e51aacff6e88992dd8fe8914
Syed Sadiq Ahamed <syedsadiqahamed198@gmail.com>
2026-03-07 08:06:10 +0530

    Basics

---
+++ Basics/largestofthreenumbers.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class largestofthreenumbers {
+    public static void main(String args[]){
+        int a=10;
+        int b=15;
+        int c=12;
+        if(a>b && a>c){
+            System.out.println("a is greater ");
+        }
+        else if(b>c){
+            System.out.println("b is greater");
+        }
+        else{
+            System.out.println("c is graeter");
+        }
+    }
+    
+}
