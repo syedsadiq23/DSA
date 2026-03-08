f6253d3d3e86ebc5da40e193e4471050ea2550b1
Syed Sadiq Ahamed <syedsadiqahamed198@gmail.com>
2026-03-08 15:31:53 +0530

    Basics

---
+++ Basics/printnumbersn.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class printnumbersn {
+    public static void main(String args[]){
+        int number=1;
+        int n=100;
+        while(number<=n){
+            System.out.print(number+" ");
+            number++;
+        }
+    }
+    
+}
