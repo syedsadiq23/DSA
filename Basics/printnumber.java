a9695eeefdd6882e4cd9a192750437dada86b95f
Syed Sadiq Ahamed <syedsadiqahamed198@gmail.com>
2026-03-08 15:31:30 +0530

    Basics

---
+++ Basics/printnumber.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class printnumber {
+    public static void main(String args[]){
+        int number=1;
+        while(number<=10){
+            System.out.print(number+" ");
+            number++;
+        }
+    }
+    
+}
