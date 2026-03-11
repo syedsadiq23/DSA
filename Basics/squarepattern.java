dbed365f1e3b3caf36110a49974b081c8be8d322
Syed Sadiq Ahamed <syedsadiqahamed198@gmail.com>
2026-03-11 05:43:15 +0530

    square pattern

---
+++ Basics/squarepattern.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class squarepattern {
+    public static void main(String args[]){
+        for(int line=1;line<=4;line++){
+            System.out.println("****");
+        }
+    }
+    
+}
