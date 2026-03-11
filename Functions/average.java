1bb0fceda78583b13392a402bac4cf77da8f3f5b
Syed Sadiq Ahamed <syedsadiqahamed198@gmail.com>
2026-03-12 00:51:31 +0530

    average

---
+++ Functions/average.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class average {
+    public static int avg(int a,int b,int c){
+        int avg=(a+b+c)/3;
+        return avg;
+    }
+    public static void main(String args[]){
+        System.out.println(avg(5,5,10));
+    }
+    
+}
