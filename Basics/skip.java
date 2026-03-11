8308306bd92d74f5c424731aa4f928a675729105
Syed Sadiq Ahamed <syedsadiqahamed198@gmail.com>
2026-03-11 05:42:29 +0530

    skip

---
+++ Basics/skip.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class skip {
+    public static void main(String args[]){
+        for(int i=1;i<=5;i++){
+            if(i==3){
+                continue;
+            }
+            System.out.println(i);
+        }
+        
+    }
+    
+}
