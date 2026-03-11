6ff49fd0e1ce566d7998d2a70a3e98372ae5550f
Syed Sadiq Ahamed <syedsadiqahamed198@gmail.com>
2026-03-11 05:44:05 +0530

    typecasting

---
+++ Basics/typecasting.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class typecasting {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        float a=25.12f;
+        int b=(int)a;
+        System.out.println(b); 
+    }
+    
+}
