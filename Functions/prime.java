c1f23a16b7236c1e8a2121499ef4c8c5cfb57138
Syed Sadiq Ahamed <syedsadiqahamed198@gmail.com>
2026-03-12 21:53:54 +0530

    prime number

---
+++ Functions/prime.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class prime {
+    public static boolean isPrime(int n){
+        boolean isPrime=true;
+        if(n==2){
+            return true;
+            
+        }
+        
+        for(int i=1;i<=Math.sqrt(n);i++){
+            if(n%i==0){
+                isPrime=false;
+                break;
+            }
+        }
+        return isPrime;
+    }
+    public static void main(String args[]){
+        System.out.println(isPrime(2));
+    }
+}
