f0087ff0c2eda653e8e36ac7cdcd6d440f406427
Syed Sadiq Ahamed <syedsadiqahamed198@gmail.com>
2026-03-13 19:50:42 +0530

    product

---
+++ Functions/product.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class product {
+    public static int product(int a,int b){
+        int p=a*b;
+        System.out.println(p);
+        return p;
+    }
+    public static void main(String args[]){
+        product(10,5);
+    }
+    
+}
