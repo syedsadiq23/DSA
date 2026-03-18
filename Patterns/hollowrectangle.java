a1baf4e4e1b3c8905e423957e0c2a26fdcfd51b2
Syed Sadiq Ahamed <syedsadiqahamed198@gmail.com>
2026-03-18 15:11:39 +0530

    rectangle

---
+++ Patterns/hollowrectangle.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class hollowrectangle {
+    public static void hollow_rect(int r,int c){
+        for(int i=1;i<=r;i++){
+            for(int j=1;j<=c;j++){
+                if(i==1||i==r||j==1||j==c){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]){
+       hollow_rect(4,5);
+    }
+}
