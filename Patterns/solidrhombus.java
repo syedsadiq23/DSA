73a98e87feec94ed06076f96a34e09abf5f23cf0
Syed Sadiq Ahamed <syedsadiqahamed198@gmail.com>
2026-03-19 12:17:03 +0530

    solid rhombus

---
+++ Patterns/solidrhombus.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class solidrhombus {
+    public static void rhombus(int n){
+        for(int i=1;i<=n;i++){
+            //spaces
+            for(int j=1;j<=(n-i);j++){
+                System.out.print(" ");
+            }
+
+            //star
+            for(int j=1;j<=n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]){
+        rhombus(5);
+    }
+    
+}
