e570ce2eda95c0a4505085fbeb40a5dee51e6a5a
Syed Sadiq Ahamed <syedsadiqahamed198@gmail.com>
2026-04-02 23:37:57 +0530

    revision

--- Basics/factorial.java
+++ Basics/factorial.java
@@ -1,15 +1,19 @@
 import java.util.*;
-public class factorial {
-    public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-        int fact=1;
-        System.out.println("Enter the positive number");
-        int n=sc.nextInt();
-        for(int i=1;i<=n;i++){
-            fact*=i;
-        }
-        System.out.println("Factorrial of the given number is:"+fact);
 
+public class Factorial {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        int fact = 1;
+        System.out.println("Enter the positive number");
+        int n = sc.nextInt();
+
+        int i = 1;
+        while (i <= n) {
+            fact *= i;
+            i++;
+        }
+
+        System.out.println("Factorial of the given number is: " + fact);
     }
-    
 }
