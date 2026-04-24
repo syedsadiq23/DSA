589ceb089c92407c0d718a50dcba1b171a1604f4
Syed Sadiq Ahamed <syedsadiqahamed198@gmail.com>
2026-04-24 11:39:30 +0530

    Count Prime

---
+++ Basics/CountPrime.java
@@ -0,0 +1,32 @@
+class CountPrime {
+    public int countPrimes(int n) {
+        if (n <= 2) return 0;
+
+        boolean[] isPrime = new boolean[n];
+
+        for (int i = 2; i < n; i++) {
+            isPrime[i] = true;
+        }
+
+        for (int i = 2; i * i < n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) count++;
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        CountPrime obj = new CountPrime();
+        int result = obj.countPrimes(10);  // change input here
+        System.out.println(result);
+    }
+}
